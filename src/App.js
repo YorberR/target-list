8b4cc16b62ec3b8e3a307c669ac581eb11db26c3
YorberR <yorberrojas15@gmail.com>
2023-02-06 17:14:17 -0400

    Dando mayor interactividad pero con nuevas fallas a corregir para ver las listas

--- src/App.js
+++ src/App.js
@@ -6,21 +6,47 @@ import { TodoItem } from './TodoItem';
 import { CreateTodoButton } from './CreateTodoButton';
 // import './App.css';
 
-const todos = [
+const defaultTodos = [
   { text: 'Comer el almuerzo', completed: false },
   { text: 'Tomar curso de react', completed: true },
   { text: 'Irme a casa', completed: false }
 ];
 
 function App() {
+  // eslint-disable-next-line no-unused-vars
+  const [todos, setTodos] = React.useState(defaultTodos);
+  const [searchValue, setSearchValue] = React.useState(' ');
+  
+  const completedTodos = todos.filter(todo => todo.completed).length;
+  const totalTodos = todos.length;
+
+  let searchedTodos = [];
+
+
+  if(!searchValue.length >= 1){
+    searchedTodos = todos;
+  } else {
+    searchedTodos = todos.filter(todo => {
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      return todoText.includes(searchText);
+    })
+  }
+
   return (
     <React.Fragment>
-      <TodoCounter />
-      <TodoSearch />
+      <TodoCounter 
+        total={totalTodos}
+        completed={completedTodos}
+      />
+      <TodoSearch 
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+      />
 
       <TodoList>
 
-        {todos.map(todo => (
+        {searchedTodos.map(todo => (
           <TodoItem 
           key={todo.text}
           text={todo.text}
--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -1,9 +1,9 @@
 import React from "react";
 import './TodoCounter.css';
 
-function TodoCounter(){
+function TodoCounter(total, completed){
     return(
-        <h2 className="TodoCounter"> Has complentado 2 de 3 ToDos</h2>
+        <h2 className="TodoCounter"> Has complentado {completed} de {total} TODOs</h2>
     );
 }
 
