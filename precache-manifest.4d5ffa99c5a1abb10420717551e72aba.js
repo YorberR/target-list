07a007fce32990cdc22df35df103942ad07aae58
YorberR <yorberrojas15@gmail.com>
2023-02-14 15:33:12 -0400

    Updates

---
+++ precache-manifest.4d5ffa99c5a1abb10420717551e72aba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a2895822f2add918e36",
+    "url": "/target-list/static/css/main.9e499e4d.chunk.css"
+  },
+  {
+    "revision": "8a2895822f2add918e36",
+    "url": "/target-list/static/js/main.8a289582.chunk.js"
+  },
+  {
+    "revision": "480a353721bd092f9455",
+    "url": "/target-list/static/js/1.480a3537.chunk.js"
+  },
+  {
+    "revision": "5d2660e5835007eee407",
+    "url": "/target-list/static/js/runtime~main.5d2660e5.js"
+  },
+  {
+    "revision": "e4c4fbf8b142cdc522b4ad1b754b14f6",
+    "url": "/target-list/index.html"
+  }
+];
