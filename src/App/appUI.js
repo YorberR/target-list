42012b2c974117727be4a286dfbec4d17dc6b234
YorberR <yorberrojas15@gmail.com>
2023-02-14 15:34:12 -0400

    taget-list

--- src/App/appUI.js
+++ src/App/appUI.js
@@ -29,7 +29,7 @@ function AppUI() {
         
         <TodoList>
             {error && <TodosError error={error} />}
-            {loading && <TodosLoading />}
+            {loading && new Array(5).fill(1).map((a, i) => <TodosLoading key={i} />)}
             {!loading && !searchedTodos.length && <EmptyTodos/>}
 
             {searchedTodos.map((todo) => (
